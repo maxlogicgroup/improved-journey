ae44dd2839705feca517cbde26800b4612fd8437
christiaanlok <61160959+christiaanlok@users.noreply.github.com>
2022-09-23 00:14:23 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -33,5 +33,5 @@ app.css.append_css({"external_url":
 "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
 if __name__ == '__main__':
-    app.run_server(debug=False)
+    app.run_server(port=8000, host='127.0.0.1')
 
